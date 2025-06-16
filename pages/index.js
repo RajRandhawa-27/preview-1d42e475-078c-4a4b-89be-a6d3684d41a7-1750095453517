811d202338078014a45b17b0bc8b091ddcff5139
RajRandhawa-27 <richrandhawa03@gmail.com>
2025-06-16 23:07:41 +0530

    Add index page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <h1>Template Preview</h1>
+      <p>Your template is being processed...</p>
+      <p>This is a placeholder page while we set up your template.</p>
+    </div>
+  )
+}
